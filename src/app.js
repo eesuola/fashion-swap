2cbaf4ac5f1202d3d34f1ac18a5376cbf53d53ff
dapp-connect <amydavid242@gmail.com>
2025-09-20 15:28:34 +0100

    Test file error

---
+++ src/app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const path = require("path");
+const sequelize = require("./config/db");
+const authRoutes = require("./routes/auth.routes");
+const userRoutes = require("./routes/user.routes");
+const itemRoutes = require("./routes/item.routes");
+const culturalPostRoutes = require("./routes/culturalPost.routes");
+const swapRequestRoutes = require("./routes/swapRequest.routes");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/items", itemRoutes);
+app.use("/api/post", culturalPostRoutes);
+app.use("/api/swaps", swapRequestRoutes);
+
+// Test DB connection
+sequelize
+  .authenticate()
+  .then(() => console.log("Database connected"))
+  .catch((err) => console.error(" Database connection failed:", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello Sequelize with CommonJS!");
+});
+
+module.exports = app; 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
+const bcrypt = require("bcrypt");
 
 const User = sequelize.define(
   "User",
