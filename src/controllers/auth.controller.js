ce74dd963170f50f335d712d1305f9cf324aabb6
dapp-connect <amydavid242@gmail.com>
2025-09-25 12:14:45 +0100

    adjusted regsiter logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -13,7 +13,7 @@ exports.register = async (req, res) => {
   try {
     const { name, email, password, location, role } = req.body;
     const avatarPath = req.file ? req.file.path : null;
-    if (!name || !email || !password || !location || !role)
+    if (!name || !email || !password || !location)
       return res.status(404).json({ error: "All field are required" });
 
     const existing = await User.findOne({ where: { email } });
