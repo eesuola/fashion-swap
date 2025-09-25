3d2b32d9e88d659fa2aa761bb5e596f8abb8464b
dapp-connect <amydavid242@gmail.com>
2025-09-25 02:23:31 +0100

    Fix database connection to use DATABASE_URL

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,15 @@ const { Sequelize } = require("sequelize");
 const isTest = process.env.NODE_ENV === "test";
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  isTest ? process.env.TEST_DB_DATABASE : process.env.DB_DATABASE || "fashion_swap",
-  isTest ? process.env.TEST_DB_USERNAME : process.env.DB_USERNAME || "postgres",
-  process.env.DB_PASSWORD || "password",
-  {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "postgres",
-    logging: console.log,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_DATABASE}`, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? {
+      require: true,
+      rejectUnauthorized: false
+    } : false
+  },
+  logging: process.env.NODE_ENV === 'production' ? false : console.log
+});
 
 module.exports = sequelize;
