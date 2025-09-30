90f4b75802e2b1967537c4fcc9206621253f4a09
dapp-connect <amydavid242@gmail.com>
2025-09-30 15:15:55 +0100

    fixed config

--- src/app.js
+++ src/app.js
@@ -9,10 +9,10 @@ const itemRoutes = require("./routes/item.routes");
 const culturalPostRoutes = require("./routes/culturalPost.routes");
 const swapRequestRoutes = require("./routes/swapRequest.routes");
 
-app.set("trust proxy", 1);
 dotenv.config();
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
